207ffcf66a90f6544ec4fd9c5cb2ddf5ab18a8b3
Aleksej Dix <git@aleksejdix.com>
2025-04-03 16:29:46 +0200

    text and date filter

--- src/components/functional/DataGrid/examples/Full/Example.tsx
+++ src/components/functional/DataGrid/examples/Full/Example.tsx
@@ -4,7 +4,12 @@ import { DataTable } from "../../components/DataTable";
 import { Transmission, transmissions } from "../data";
 import { SortableHeaderCell } from "../../components/SortableHeaderCell";
 import { useMemo } from "react";
-import { useQueryState, parseAsStringLiteral } from "nuqs";
+import {
+  useQueryState,
+  parseAsStringLiteral,
+  parseAsTimestamp,
+  parseAsIsoDate,
+} from "nuqs";
 import { RowModel } from "../../components/RowModel";
 import { HeaderGroups } from "../../components/HeaderGroups";
 import { ColumnVisibility } from "../../components/ColumnVisibility";
@@ -12,6 +17,8 @@ import { ColumnOrdering } from "../../components/ColumnOrdering";
 import { createStatusCell } from "../../components/custom-cells/Status";
 
 import { createDateCell } from "../../components/custom-cells/Date";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
 
 const columnHelper = createColumnHelper<Transmission>();
 
@@ -23,6 +30,31 @@ export const Example = () => {
     "createdAt",
     parseAsStringLiteral(sortOrder)
   );
+
+  const [referenceFilter, setReferenceFilter] = useQueryState(
+    "correlationReference"
+  );
+
+  const [fromDateFilter, setFromDateFilter] = useQueryState("from");
+  const [toDateFilter, setToDateFilter] = useQueryState("to");
+
+  function updateFromDateFilter(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    if (value === "") {
+      setFromDateFilter(null);
+    } else {
+      setFromDateFilter(value);
+    }
+  }
+
+  function updateToDateFilter(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    if (value === "") {
+      setToDateFilter(null);
+    } else {
+      setToDateFilter(value);
+    }
+  }
 
   const columns = useMemo(
     () => [
@@ -65,18 +97,48 @@ export const Example = () => {
   );
 
   return (
-    <DataGrid
-      columns={columns as ColumnDef<Transmission>[]}
-      data={transmissions}
-    >
-      <div className="flex justify-between">
-        <ColumnOrdering />
-        <ColumnVisibility />
-      </div>
-      <DataTable>
-        <HeaderGroups />
-        <RowModel />
-      </DataTable>
-    </DataGrid>
+    <>
+      <form>
+        <div>
+          <Label>Reference</Label>
+          <Input
+            type="text"
+            value={referenceFilter ?? ""}
+            onChange={(e) => setReferenceFilter(e.target.value)}
+          />
+        </div>
+        <div>
+          <Label>From Date</Label>
+          <Input
+            id="fromDate"
+            type="date"
+            value={fromDateFilter ?? ""}
+            onChange={updateFromDateFilter}
+          />
+        </div>
+        <div>
+          <Label>To Date</Label>
+          <Input
+            id="toDate"
+            type="date"
+            value={toDateFilter ?? ""}
+            onChange={updateToDateFilter}
+          />
+        </div>
+      </form>
+      <DataGrid
+        columns={columns as ColumnDef<Transmission>[]}
+        data={transmissions}
+      >
+        <div className="flex justify-between">
+          <ColumnOrdering />
+          <ColumnVisibility />
+        </div>
+        <DataTable>
+          <HeaderGroups />
+          <RowModel />
+        </DataTable>
+      </DataGrid>
+    </>
   );
 };
