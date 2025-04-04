7944ff02bcdcc4291873dd0d581070f37d094b18
Aleksej Dix <git@aleksejdix.com>
2025-04-04 14:18:15 +0200

    fix data grid

--- src/components/functional/DataGrid/index.ts
+++ src/components/functional/DataGrid/index.ts
@@ -6,7 +6,4 @@ export { DataTable } from "./components/DataTable";
 export { type RowData, useDataGrid } from "./components/DataGrid.types";
 
 // Row model for headless rendering
-export { TableRows } from "./components/RowModel";
-
-// Table components
-export * from "./components/table";
+export { RowModel } from "./components/RowModel";
